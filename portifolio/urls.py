49414f0b838cde840ab40aed7d6a493abd3dbfdd
Antonio Lindo <antoniolvitall@gmail.com>
2023-01-21 12:38:53 -0300

    link de url

--- portifolio/urls.py
+++ portifolio/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from portifolio.views import index, imagem
 
 urlpatterns = [
-    path('', index),
-    path('imagem/', imagem)
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem'),
 ]
